3bc0bd86a2c816503954717b7170e2ad11b69d44
Patrice Briggs <PBriggs@perkinscoie.com>
2019-05-21 13:20:59 -0700

    updated dacpac

--- BookClubDB/dbo/Stored Procedures/_sp_AddBookSource.sql
+++ BookClubDB/dbo/Stored Procedures/_sp_AddBookSource.sql
@@ -4,7 +4,7 @@
 -- Description:	Add Book Source
 -- exec _sp_AddBookSource 'Reese Witherspoon''s Book Club Picks', 'https://www.readitforward.com/essay/article/reese-witherspoons-book-club/'
 -- =============================================
-CREATE PROCEDURE _sp_AddBookSource
+CREATE PROCEDURE [dbo].[_sp_AddBookSource]
 	@bookSourceName nvarchar(100),
 	@BookSourceLink nvarchar(200)
 AS
@@ -18,5 +18,4 @@ BEGIN
 	Values
 	(@bookSourceName, @BookSourceLink)
 
-	Select * FROM BookSources
 END
---
+++ BookClubDB/dbo/Stored Procedures/_sp_GetGenres.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		BRIGP
+-- Create date: 5/20/2019
+-- Description:	get alphabetical list of genres
+-- =============================================
+CREATE PROCEDURE _sp_GetGenres
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	SELECT * FROM Genres
+	Order By GenreDesc ASC
+END
